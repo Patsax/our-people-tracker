1fcf75fb584df7f7ca4290ddde71404a34483e3f
Peter Thao <peterthao@Peters-iMac.local>
2021-11-12 19:51:26 -0500

    created schema.sql and edit seeds.sql

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS managers;
+
+
+
+CREATE TABLE managers(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    complete_name VARCHAR(30)  
+);
+
+
+CREATE TABLE evalutions(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    attendance INTEGER,
+    work_quality INTEGER,
+    productivity INTEGER,
+    communication INTEGER,
+    job_knowledge INTEGER,
+    employee_id INTEGER;
+);
+
+CREATE TABLE employees(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    employee_id INTEGER,
+    manager_id INTEGER,
+    roles VARCHAR(30) NOT NULL,
+    salary INTEGER,
+    date_hired DATE NOT NULL,
+    CONSTRAINT fk_roles FOREIGN KEY (employee_id) REFERENCES evalutions(employee_id) ON DELETE SET NULL,
+    CONSTRAINT fk_managers FOREIGN KEY (manager_id) REFERENCES managers(id) ON DELETE SET NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -8,25 +8,25 @@ VALUES
 
 INSERT INTO employees (first_name, last_name, employee_id, manager_id, roles, salary, date_hired)
 VALUES
-('John', 'Denver', 2, 5, 'Engineer', 85000, 2001-01-31 ),
-('Mike', 'Chang', 6, 2, 'Sales', 110000),
-('Ashley', 'Rodriguez', 3, 4, 'Quality Control', 70000, 2003-02-14),
-('Sherry', 'Hungsinger', 7, 1, 'Janitor', 50000),
-('Robert', 'Eli', 1, 4, 'Vice President', 200000 ),
-('Sarah', 'Peterson', 4, 2, 'President', 250000),
-('Thomas', 'Chamberlin', 2, 4, 'Human Resources', 80000),
-('James', 'Bonder', 4, 2, 'President', 250000),
-('Jack', 'Hoffer', 2, 4, 'Human Resources', 80000),
-('Christian', 'McCaffery', 3, 3 'CEO/COO', 400000);
+('John', 'Denver', 601, 3, 'Engineer', 85000, 2001-01-31 ),
+('Mike', 'Chang', 502, 2, 'Sales', 110000, 2002-02-14),
+('Ashley', 'Rodriguez', 402, 4, 'Quality Control', 70000, 2003-02-14),
+('Sherry', 'Hungsinger', 933, 5, 'Janitor', 50000, 2020-08-20),
+('Robert', 'Eli', 501, 2, 'Sales', 200000, 1999-07-05),
+('Sarah', 'Peterson', 202, 1, 'President', 250000, 1993-05-23),
+('Thomas', 'Chamberlin', 406, 4, 'Human Resources', 80000, 1996-06-21),
+('James', 'Bonder', 301, 1, 'Vice-President', 170000, 1993-06-02),
+('Jack', 'Hoffer', 404, 4, 'Human Resources', 80000, 1995-03-02),
+('Christian', 'McCaffery', 101, 1,'CEO/COO', 400000, 1993-05-22);
 
-INSERT INTO evalutions (employee_id, attendance, work_quality, productivity, communication, job_knowledge) VALUES
-(1, 3, 3, 4, 2, 3),
-(2, 2, 3, 4, 4, 3),
-(3, 3, 3, 4, 5, 2),
-(4, 3, 5, 4, 2, 3),
-(5, 2, 3, 4, 2, 4),
-(6, 4, 3, 4, 3, 3),
-(7, 2, 4, 4, 1, 4),
-(8, 3, 4, 4, 5, 5),
-(9, 3, 4, 4, 5, 5),
-(10, 3, 3, 4, 2, 3);
+INSERT INTO evalutions (attendance, work_quality, productivity, communication, job_knowledge) VALUES
+(3, 3, 4, 2, 3, 502),
+(2, 3, 4, 4, 3, 933),
+(3, 3, 4, 5, 2, 202),
+(3, 5, 4, 2, 3, 406),
+(2, 3, 4, 2, 4, 404),
+(4, 3, 4, 3, 3, 101),
+(2, 4, 4, 1, 4, 301),
+(3, 4, 4, 5, 5, 501),
+(3, 4, 4, 5, 5, 601),
+(3, 3, 4, 2, 3, 402);
