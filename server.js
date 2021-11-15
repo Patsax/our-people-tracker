0490eda7e01eb09e620785190b7b51dfad348f88
Derick Hooley <dghooley@gmail.com>
2021-11-14 22:47:53 -0600

    bugFixes/Post

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const db = require('./models');
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
