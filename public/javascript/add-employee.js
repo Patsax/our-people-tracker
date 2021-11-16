00fd3974d2e81030ab58bebc656e7cf22dcf46e3
Patrick <patasax22@yahoo.com>
2021-11-15 19:23:03 -0500

    add, delete, and edit employee.js added

---
+++ public/javascript/add-employee.js
@@ -0,0 +1,27 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    const firstName = document.querySelector('input[name="first-name"]').value;
+    const lastName = document.querySelector('input[name="last-name"]').value;
+    const role = document.querySelector('input[name="role"]').value;
+
+    const response = await fetch('/api/employees', {
+        method: 'POST',
+        body: JSON.stringify({
+            firstName,
+            lastName,
+            role
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/homepage');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.new-employee-form').addEventListener('submit', newFormHandler);
---
+++ public/javascript/edit-employee.js
@@ -0,0 +1,29 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const firstName = document.querySelector('input[name="first-name"]').value.trim();
+    const lastName = document.querySelector('input[name="last-name"]').value.trim();
+    const role = document.querySelector('input[name="role"]').value.trim();
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch(`/api/employees/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            firstName,
+            lastName,
+            role
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/homepage/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.edit-employee-form').addEventListener('submit', editFormHandler);
