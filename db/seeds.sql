82a5ff7fd12d9ca65a109c310f5f51600c97bc39
Patrick <patasax22@yahoo.com>
2021-11-20 16:33:23 -0500

    file and code cleanup

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,20 +18,18 @@ VALUES
 
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES
-    INSERT INTO employee (id, first_name, last_name, hired, salary, role_id)
-VALUES
-    ('Rick', 'Pick', 01/23/4567, 100000, 1),
-    ('Zoey', 'Nelson', 01/23/4567, 80000, 2),
-    ('Morty', 'Henderson', 01/23/4567, 80000, 2),
-    ('Richard', 'Rahl', 01/23/4567, 80000, 2),
-    ('Kahlan', 'Amnell', 01/23/4567, 150000, 3),
-    ('Cara', 'Doon', 01/23/4567, 120000, 4),
-    ('Felix', 'Felder', 01/23/4567, 120000, 4),
-    ('Jaina', 'Neidmeier', 01/23/4567, 125000, 5),
-    ('Cora', 'Namor', 01/23/4567, 125000, 5),
-    ('Kimberly', 'Feldon', 01/23/4567, 125000, 5),
-    ('Gemma', 'Simmons', 01/23/4567, 250000, 6),
-    ('Rebecca', 'Meier', 01/23/4567, 190000, 7),
-    ('Albert', 'Wesker', 01/23/4567, 190000, 7),
-    ('Misha', 'Yovovich', 01/23/4567, 190000, 7);
+    ('Rick', 'Pick', 1),
+    ('Zoey', 'Nelson', 2),
+    ('Morty', 'Henderson', 2),
+    ('Richard', 'Rahl', 2),
+    ('Kahlan', 'Amnell', 3),
+    ('Cara', 'Doon', 4),
+    ('Felix', 'Felder', 4),
+    ('Jaina', 'Neidmeier', 5),
+    ('Cora', 'Namor', 5),
+    ('Kimberly', 'Feldon', 5),
+    ('Gemma', 'Simmons', 6),
+    ('Rebecca', 'Meier', 7),
+    ('Albert', 'Wesker', 7),
+    ('Misha', 'Yovovich', 7);
 
